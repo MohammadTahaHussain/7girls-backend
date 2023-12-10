82cb0ab4cc00a41f04f671aeec7159b92058de5c
MohammadTahaHussain <hussaintaha620@gmail.com>
2023-12-10 15:57:33 +0500

    added get route

--- routes/Auth.mjs
+++ routes/Auth.mjs
@@ -2,6 +2,13 @@ import express from "express";
 const router = express.Router();
 import { createUser, LoginUser, checkOut } from "../controller/Auth.mjs";
 
+router.get('/', (req, res) => {
+  res.status(200).send({
+    Name: 'Taha',
+    Age : 18
+  })
+})
+
 router.post("/register", (req, res) => {
   createUser(req, res);
 });
